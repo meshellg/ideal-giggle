b9524b7f0e812ae797a124cbfbd158124160095c
meshellg <meshell@meshell.ca>
2021-11-05 14:44:48 -0400

    eleventy img addition

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
 const pluginTailwind = require('eleventy-plugin-tailwindcss');
+const Image = require("@11ty/eleventy-img");
+
+(async () => {
+  let url = "https://images.unsplash.com/photo-1608178398319-48f814d0750c";
+  let stats = await Image(url, {
+    widths: [300]
+  });
+
+  console.log( stats );
+})();
 
 module.exports = (config) => {
   config.addPlugin(pluginTailwind, {
     src: 'src/assets/css/*'
   });
+
 
   config.setDataDeepMerge(true);
 
