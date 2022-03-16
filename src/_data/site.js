9190052259f0dbd922fce1453aca7b787f07d6c4
meshellg <meshell@meshell.ca>
2022-03-16 11:46:41 -0400

    9page

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,7 +5,7 @@ module.exports = {
   // your site url without trailing slash
   url: 'https://meshell.ca',
   // how many posts you want to show for each page
-  paginate: 6
+  paginate: 9
   // if you want to add disqus to your site
   // disqusShortname: "your-shortname"
 };
